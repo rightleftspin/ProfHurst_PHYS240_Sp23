72950aa7a3882025c374b411eabb3360417a2278
Pranav Seetharaman <pranav@myrdd.info>
2023-02-01 16:49:55 -0800

    Hello PHYS 240 Submission (#6)
    * Initial Commit with flake.nix for nixos and Hello 240 Python Code
    
    * Editing Hello file to add Name and Color
    
    * Adding direnv to local system
    
    * Matthew and Pranav's submission for Lab 1

---
+++ Seetharaman/flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    pypi-deps-db = {
+      url = "github:DavHau/pypi-deps-db/4be31ec67b9283eac29494920588a9748eb06d18";
+      flake = false;
+    };
+    mach-nix = {
+	url = "mach-nix/3.5.0";
+	inputs.pypi-deps-db.follows = "pypi-deps-db"; 
+    };
+  };
+
+  outputs = {self, nixpkgs, mach-nix, pypi-deps-db }@inp:
+    let
+      l = nixpkgs.lib // builtins;
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      forAllSystems = f: l.genAttrs supportedSystems
+        (system: f system (import nixpkgs {inherit system;}));
+	 
+    in
+    {
+      # enter this python environment by executing `nix shell .`
+      devShell = forAllSystems (system: pkgs: mach-nix.lib."${system}".mkPythonShell {
+        requirements = ''
+          numpy
+	  scipy
+          matplotlib
+	  sympy
+        '';
+      });
+    };
+}
+
