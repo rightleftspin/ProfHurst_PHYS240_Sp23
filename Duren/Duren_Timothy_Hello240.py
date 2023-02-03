9270f10cb421f46519826fa757e2aefdc7148030
TED <43099947+TE-Duren@users.noreply.github.com>
2023-02-03 15:10:21 -0800

    Student submissions (#18)
    * Create git
    
    * Create Duren_Timothy_Hello240.py
    
    Hello world file
    
    ---------
    
    Co-authored-by: TED <43099947+JediKnight91@users.noreply.github.com>

---
+++ Duren/Duren_Timothy_Hello240.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+sentence = ' '
+#join is an iterable
+new_sentence = sentence.join(
+    ['Hello,'
+    ,'World'])
+
+
+# In[5]:
+
+
+print(new_sentence)
+
